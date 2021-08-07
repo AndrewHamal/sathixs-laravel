cb75b948772f70361462a4f67bf3cbbd63c2aad0
AndrewHamal <andrew@revealize.com>
2021-08-08 02:36:05 +0545

    changes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => false,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'encrypted' => true,
+                'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_APP_PORT', 6001),
+                'scheme' => 'https'
             ],
         ],
 
